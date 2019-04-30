f5899d7d5dd5669a07c2efb14d7b69c2794f9257
fxfxrxfx <santi-c-b@outlook.com>
2019-04-30 03:49:46 -0500

    Not much by now

---
+++ gameboy.js
@@ -0,0 +1,147 @@
+
+/**
+ * Most of the registers are set by default with a non-real size.
+ * Most of the registers should be 8bit variables, but that's something that's out of my hands
+ * using Javascript. So...
+ */
+class Gameboy {
+    constructor(){
+        //8 bits register. Accumulator. The only one that can do arithmetic operations
+        this.A = 0x00;
+        //8 bits register. Flags. Indicates the processor status.
+        this.F = 0x00;
+        //8 bits registers for general use.
+        this.B = 0x00;
+        this.C = 0x00;
+        this.D = 0x00;
+        this.H = 0x00;
+        this.E = 0x00;
+        this.L = 0x00;
+        //16 bit. Stack Pointer. Indicates the address in memory of the element at the top of the stack.
+        this.stackPointer = 0xFFEE;
+        //16 bit. Program Counter. Indicates the address in memory to exec.
+        this.pc = 0x0100;
+        //Gameboy has an address space of 64k. Divided id:
+        //[0000-7FFF] ROM: Game loaded. Divided in:
+        //  ROM0: Static. Redirects to game base.
+        //  ROM1: Has memory banks, that can be exchange using "Memory banking"
+        //  [0000-00FF] BOOT ROM / BIOS: Cleans RAM, writes Nintendo logo, plays "GAMEBOY" sound.
+        //  [0100-014F] Header: Points to header with metadata of the game (Game, reqs, etc).
+        //[8000-9FFF] Video RAM: Sprites and backgrounds.
+        //[A000-BFFF] External RAM: Optional. Points to RAM's chip from the game.
+        //[C000-DFFF] Work RAM: 8k. Internal RAM of the console. Can be read and written.
+        //[E000-FDFF] "Mirror" zone: These addresses points to Work RAM. It's not convenient their use.
+        //[FE00-FFFF] Have some subdivs:
+        //  [FE00-FE9F] OAM (Object Attribute Memory) RAM: Saves properties of sprites (Position, transparency, etc).
+        //  [FEA0-FEFF] Reserved. Not used for devs.
+        //  [FF00-FF7F] I/O: Controls input and outpus
+        //  [FF80-FFFE] High RAM: Fastest RAM. 
+        //  [FFFF] System interruptions.
+        this.memory = new Uint8Array(1024 * 64);
+
+        this.display = new Uint8Array(160 * 144);
+    }
+
+
+    loadProgram(data) {
+        for (let i = 0x0000; i < 0x7FFF; i++){
+            const instruction = data[i];
+            this.memory[i] = instruction
+        }
+        console.log("Memory: ", this.memory);
+    }
+
+    //////////////////////
+    //  FLAG REGISTER   //
+    //////////////////////
+    //  7 6 5 4 3 2 1 0 //
+    //  Z N H C 0 0 0 0 //
+    //////////////////////
+
+    /**
+     * Zero Flag (Z):
+     * This bit is set when the result of a math operation
+     * is zero or two values match when using the CP
+     * instruction.
+     * @param bool 
+     */
+    setZeroFlag(bool){
+        const bit = bool ? 1 : 0;
+        this.F |= bit << 7;
+    }
+
+    /**
+     * ï Subtract Flag (N):
+     * This bit is set if a subtraction was performed in the
+     * last math instruction
+     */
+    setSubstractFlag(bool){
+        const bit = bool ? 1 : 0;
+        this.F |= bit << 6;
+    }
+
+    /**
+     * Half Carry Flag (H): 
+     * This bit is set if a carry occurred from the lower
+     * nibble in the last math operation.
+     * @param  bool 
+     */
+    setHalfCarryFlag(bool){
+        const bit = bool ? 1 : 0;
+        this.F |= bit << 5;
+    }
+
+    /**
+     * ï Carry Flag (C):
+     * This bit is set if a carry occurred from the last
+     * math operation or if register A is the smaller value
+     * when executing the CP instruction.
+     * @param bool 
+     */
+    setCarryFlag(bool) {
+        const bit = bool ? 1 : 0;
+        this.F |= bit << 4;
+    }
+
+    setByteFlag(value) {
+        this.F | value;
+    }
+
+    run(){
+        //To get opcode we get the memory at the program counter (PC)
+        //We carry those bytes 8 bits to left ( << operator )
+        //Then we add the next ones pair of bytes to it ( | OR Operator )
+        const opcode = this.memory[this.pc];
+        this.pc++;
+        const hexOpcode = opcode.toString(16);
+        console.log("OPCODE:", hexOpcode);
+
+        switch (opcode) {
+            case 0x00: {
+                //Don't do anything...
+                break;
+            }
+
+            //Increase B
+            case 0x04: {
+                const cycles = 4;
+                for (let i = 0; i < cycles; i++) {
+                    this.B = (this.B + 0x01) & 0xFF;    
+                }    
+                break;
+            }
+
+            //RLCA
+            //Rotate A left. Old bit 7 to Carry flag.
+            case 0x07: {
+
+            }
+
+            case 0x7F: {
+                const cycles = 4;
+
+            }
+        }
+    }
+
+}
---
+++ main.js
@@ -0,0 +1,100 @@
+var iter = 0;
+
+/**
+ * Gets file input from element #file at HTML
+ * Uses FileReader to read that file ArrayBuffer when the file's read'n loaded.
+ * Finally, converts that arrayBuffer to a Byte array and sends it to chip to be loaded. 
+ */
+function loadFile(){
+    const file = document.getElementById('file').files[0];
+    const reader  = new FileReader();
+    reader.onload = function () {
+        const arrayBuffer = reader.result;
+        const bArray = new Uint8Array(arrayBuffer);
+        gameboy.loadProgram(bArray);
+        setInterval(start, 1000);
+    }
+    reader.readAsArrayBuffer(file);
+}
+   
+
+function fitToContainer(canvas){
+    // Make it visually fill the positioned parent
+    canvas.style.width ='100%';
+    canvas.style.height='100%';
+    // ...then set the internal size to match
+    canvas.width  = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+}
+
+/**
+ * Get the display from the chip and loops it to check the pixels and paint it.
+ */
+function redrawCanvas(){
+    /**
+     * Draws a pixel filling a lil rect of size of canvas in relation with console display size at {x, y}
+     * @param coor 
+     * @param color 
+     */
+    const drawPixel = (coor, color) => {
+        const { x, y } = coor;
+        ctx.fillStyle = color;
+        ctx.fillRect(x * PIXEL_WIDTH, y * PIXEL_HEIGHT, PIXEL_WIDTH, PIXEL_HEIGHT);
+    }
+
+    const { display } = gameboy;
+    display.forEach( (pixel, index) => {
+        const coor = {
+            x: index % 160,
+            y: parseInt(index / 160) 
+        };
+        drawPixel(coor, ( pixel == 1 ) ? "green" : "black");
+    })
+
+}
+
+function start(){
+    gameboy.run();
+    // if(chip) {
+    //     if(chip.needsRedraw) {
+    //         redrawCanvas();
+    //     }
+    //     chip.setKeyBuffer(keyboard.getKeyBuffer());
+    //     chip.run();
+    // }
+    
+}
+
+
+function addListeners(){
+    const getKey = keyCode => String.fromCharCode(keyCode);
+    document.addEventListener('keydown', event => {
+        keyboard.keyPressed(getKey(event.keyCode));
+    } );
+
+    document.addEventListener('keyup', event => {
+        keyboard.keyReleased(getKey(event.keyCode));
+    } );
+}
+
+function initEmulator(){
+    //Fullscreen canvas
+    redrawCanvas();
+    //addListeners();
+}
+
+
+
+//Getting canvas
+const canvas = document.getElementById("canvas");
+fitToContainer(canvas);
+//Useful constants
+const PIXEL_WIDTH = canvas.width / 160;
+const PIXEL_HEIGHT =  canvas.height / 144;
+//Getting context
+const ctx = canvas.getContext('2d');
+//Init chip
+let gameboy = new Gameboy();
+
+
+initEmulator()
